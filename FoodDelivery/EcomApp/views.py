d019153546a3e356296c63ae6a017cc1d270d8b8
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-04-03 17:50:49 +0600

    file separeted

--- FoodDelivery/EcomApp/views.py
+++ FoodDelivery/EcomApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
+#email:rafiqul.pust.cse@gmail.com pass:rafiq123
 # Create your views here.
 def Home(request): 
     context={}
-    return render(request, 'index.html',context)
+    return render(request, 'home.html',context)
